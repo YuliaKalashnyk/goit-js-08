f95e290ffc99aa248d5b65d6b41d52b7627af9e1
Yulia Kalashnyk <jkalashnik.97@gmail.com>
2023-04-08 00:22:51 +0300

    завдання 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,67 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+console.log(gallery);
+
+const listItemMarkup = createListGalery(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', listItemMarkup);
+
+function createListGalery (galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+      <li class="gallery__item">
+        <a
+          class="gallery__link"
+          href = '${original}';
+        >
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+      `;
+      }).join('');
+};
+
+gallery.addEventListener('click', onImgClick);
+
+function onImgClick(evt) {
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const modal = basicLightbox.create(`<img src="${evt.target.dataset.source}">`,
+        { onShow: () => window.addEventListener('keydown', onEscKeyPress),
+        onClose: () => window.removeEventListener('keydown', onEscKeyPress),
+        }
+    );
+    modal.show();
+
+    function onEscKeyPress(evt) {
+        if (evt.code === "Escape") {
+            modal.close();
+        }
+    }
+};
+
+new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+  
+
